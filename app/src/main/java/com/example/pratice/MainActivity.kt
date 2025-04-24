cc592298d399446600aed0968758611a4bb0bccf
Thanh Hoang <thanhhoang1001@hotmail.com>
2025-04-25 03:47:18 +0700

    change currency

--- app/src/main/java/com/example/pratice/MainActivity.kt
+++ app/src/main/java/com/example/pratice/MainActivity.kt
@@ -1,136 +1,108 @@
 package com.example.pratice
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var editTextA: EditText
-    private lateinit var editTextB: EditText
-    private lateinit var editTextC: EditText
-    private lateinit var editTextResult: EditText
-    private lateinit var buttonSolve: Button
+    private val currencyList = listOf("USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "KRW", "VND")
+    private val rates = mapOf(
+        "USD" to 1.0,
+        "EUR" to 0.9,
+        "JPY" to 150.0,
+        "GBP" to 0.8,
+        "AUD" to 1.4,
+        "CAD" to 1.35,
+        "CHF" to 0.95,
+        "CNY" to 7.2,
+        "KRW" to 1350.0,
+        "VND" to 25000.0
+    )
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.bai1)
-        editTextA = findViewById(R.id.editTextA)
-        editTextB = findViewById(R.id.editTextB)
-        editTextC = findViewById(R.id.editTextC)
-        editTextResult = findViewById(R.id.editTextResult)
-        buttonSolve = findViewById(R.id.buttonSolve)
+        //enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val spinnerFrom = findViewById<Spinner>(R.id.spinnerFrom)
+        val spinnerTo = findViewById<Spinner>(R.id.spinnerTo)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencyList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerFrom.adapter = adapter
+        spinnerTo.adapter = adapter
 
-        buttonSolve.setOnClickListener {
-            solveQuadraticEquation()
-        }
+        var isEditingFrom = false
+        var isEditingTo = false
 
-    }
+        val editFrom = findViewById<EditText>(R.id.editFrom)
+        val editTo = findViewById<EditText>(R.id.editTo)
 
 
-    
-    private fun solveQuadraticEquation() {
-        val aText = editTextA.text.toString()
-        val bText = editTextB.text.toString()
-        val cText = editTextC.text.toString()
+        editFrom.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
-        if (aText.isEmpty() || bText.isEmpty() || cText.isEmpty()) {
-            editTextResult.setText("Vui lòng nhập đầy đủ a, b, c.")
-            return
-        }
-
-        try {
-            val a = aText.toDouble()
-            val b = bText.toDouble()
-            val c = cText.toDouble()
-
-            val result = when {
-                a == 0.0 -> {
-                    if (b == 0.0) {
-                        if (c == 0.0) "Phương trình vô số nghiệm"
-                        else "Phương trình vô nghiệm"
-                    } else {
-                        val x = -c / b
-                        "Phương trình bậc nhất\nNghiệm x = $x"
-                    }
-                }
-                else -> {
-                    val delta = b * b - 4 * a * c
-                    when {
-                        delta < 0 -> "Phương trình vô nghiệm"
-                        delta == 0.0 -> {
-                            val x = -b / (2 * a)
-                            "Phương trình có nghiệm kép:\nx = $x"
-                        }
-                        else -> {
-                            val sqrtDelta = Math.sqrt(delta)
-                            val x1 = (-b + sqrtDelta) / (2 * a)
-                            val x2 = (-b - sqrtDelta) / (2 * a)
-                            "Phương trình có 2 nghiệm:\nx1 = $x1\nx2 = $x2"
-                        }
-                    }
-                }
             }
 
-            editTextResult.setText(result)
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isEditingTo) return
+                isEditingFrom = true
+                val from = spinnerFrom.selectedItem.toString()
+                val to = spinnerTo.selectedItem.toString()
+                val amountText = editFrom.text.toString()
+                val amount = amountText.toDoubleOrNull() ?: return
+                val result = convertCurrency(amount, from, to)
+                val formattedResult = String.format("%.2f", result)
 
-        } catch (e: NumberFormatException) {
-            editTextResult.setText("Hệ số phải là số hợp lệ.")
-        }
+                editTo.setText(formattedResult)
+
+                isEditingFrom = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+
+        editTo.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (isEditingFrom) return
+                isEditingTo = true
+                val from = spinnerTo.selectedItem.toString()
+                val to = spinnerFrom.selectedItem.toString()
+                val amountText = editTo.text.toString()
+                val amount = amountText.toDoubleOrNull() ?: return
+                val result = convertCurrency(amount, from, to)
+                val formattedResult = String.format("%.2f", result)
+                editFrom.setText(formattedResult)
+                isEditingTo = false
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+        })
     }
 
-    // private lateinit var editTextInput: EditText
-    // private lateinit var checkUppercase: CheckBox
-    // private lateinit var checkLowercase: CheckBox
-    // private lateinit var checkReverse: CheckBox
-    // private lateinit var textViewResult: TextView
 
-    // override fun onCreate(savedInstanceState: Bundle?) {
-    //     super.onCreate(savedInstanceState)
-    //     setContentView(R.layout.activity_main)
+    private fun convertCurrency(amount: Double, from: String, to: String): Double {
+        val fromRate = rates[from] ?: 1.0
+        val toRate = rates[to] ?: 1.0
+        return (amount / fromRate) * toRate
+    }
 
-    //     editTextInput = findViewById(R.id.editTextInput)
-    //     checkUppercase = findViewById(R.id.checkUppercase)
-    //     checkLowercase = findViewById(R.id.checkLowercase)
-    //     checkReverse = findViewById(R.id.checkReverse)
-    //     textViewResult = findViewById(R.id.textViewResult)
 
-    //     // Gắn sự kiện thay đổi
-    //     val onChangeListener = {
-    //         applyTransform()
-    //     }
-
-    //     editTextInput.addTextChangedListener(object : TextWatcher {
-    //         override fun afterTextChanged(s: Editable?) = onChangeListener()
-    //         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-    //         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-    //     })
-
-    //     checkUppercase.setOnCheckedChangeListener { _, _ -> onChangeListener() }
-    //     checkLowercase.setOnCheckedChangeListener { _, _ -> onChangeListener() }
-    //     checkReverse.setOnCheckedChangeListener { _, _ -> onChangeListener() }
-    // }
-
-    // private fun applyTransform() {
-    //     var result = editTextInput.text.toString()
-
-    //     if (checkUppercase.isChecked) {
-    //         result = result.uppercase()
-    //     }
-
-    //     if (checkLowercase.isChecked) {
-    //         result = result.lowercase()
-    //     }
-
-    //     if (checkReverse.isChecked) {
-    //         result = result.reversed()
-    //     }
-
-    //     textViewResult.text = result
-    // }
 
 }
