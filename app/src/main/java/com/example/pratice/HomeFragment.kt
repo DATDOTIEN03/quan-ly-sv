390e73307547970a9639820cf0ad96ee59f0f4cc
Do Tien Dat <thanhhoang1001@hotmail.com>
2025-06-08 00:55:43 +0700

    Fragment

--- app/src/main/java/com/example/pratice/HomeFragment.kt
+++ app/src/main/java/com/example/pratice/HomeFragment.kt
@@ -18,7 +18,6 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
         viewModel = ViewModelProvider(requireActivity())[StudentViewModel::class.java]
